9bf383f04b2e1af7b88c4928ea811c633cdfdd31
grancredfinanzas-design <grancred.finanzas@gmail.com>
2026-08-04 20:23:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "grancred-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
